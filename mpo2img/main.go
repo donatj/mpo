ebc1e181d35b8e0f1f467d561692f8f3340745b4
Jesse Donat <donatj@gmail.com>
2015-04-15 22:17:49 -0500

    Adds basic mpo2img CLI Tool

---
+++ mpo2img/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/donatj/mpo"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+func init() {
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+}
+
+var (
+	format = flag.String("format", "stereo", "Output format [stereo|red-cyan|cyan-red|red-green|green-red]")
+	output = flag.String("outfile", "output.png", "Output filename")
+	help   = flag.Bool("help", false, "Displays this text")
+)
+
+func init() {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		fmt.Println("usage: mpo2img <mpofile>")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	m, err := mpo.Decode(flag.Arg(0))
+	if err != nil {
+		log.Fatalf("err on %v %s", err, flag.Arg(0))
+	}
+
+	var img image.Image
+	switch *format {
+	case "stereo":
+		img = m.ConvertToStereo()
+	case "red-cyan":
+		img, err = m.ConvertToAnaglyph(mpo.RedCyan)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "cyan-red":
+		img, err = m.ConvertToAnaglyph(mpo.CyanRed)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "red-green":
+		img, err = m.ConvertToAnaglyph(mpo.RedGreen)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "green-red":
+		img, err = m.ConvertToAnaglyph(mpo.GreenRed)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Unknown format:", *format)
+	}
+
+	f, err := os.OpenFile(*output, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = png.Encode(f, img); err != nil {
+		log.Fatal(err)
+	}
+}
